669e134b480b034fe40aa9df0dd17301ccf1a7a1
cchuan <caizhengchuan@163.com>
2016-01-15 16:48:11 +0800

    添加工具h文件

---
+++ Cc_ToolProject/Cc_ToolProject/Tools.h
@@ -0,0 +1,15 @@
+//
+//  Tools.h
+//  Cc_ToolProject
+//
+//  Created by cchuan on 16/1/15.
+//  Copyright © 2016年 cchuan. All rights reserved.
+//
+
+#ifndef Tools_h
+#define Tools_h
+
+#define kUserDefault [NSUserDefaults standardUserDefaults]
+#define kUserDefault_syn [[NSUserDefaults standardUserDefaults] synchronize]
+
+#endif /* Tools_h */
